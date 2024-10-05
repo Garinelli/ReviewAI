e7cb09278be71290ac63db03e8fd92100a91496d
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-10-06 00:17:06 +0700

    Add fake reviews.

---
+++ parser/fake_reviews.py
@@ -0,0 +1,82 @@
+fake_reviews = [
+    {
+        "user_review": "Отличный товар, всем рекомендую! Качество на высоте, буду заказывать ещё.",
+        "reviews_date": "2024-09-30",
+        "star_review": 5,
+        "text_len": 66,
+        "written_by_bot": 1,
+        "has_media": True
+    },
+    {
+        "user_review": "Превзошло все мои ожидания! Советую всем друзьям.",
+        "reviews_date": "2024-09-29",
+        "star_review": 5,
+        "text_len": 51,
+        "written_by_bot": 1,
+        "has_media": False
+    },
+    {
+        "user_review": "Купил и остался доволен, за такие деньги лучше не найти.",
+        "reviews_date": "2024-09-28",
+        "star_review": 5,
+        "text_len": 54,
+        "written_by_bot": 1,
+        "has_media": False
+    },
+    {
+        "user_review": "Очень хорошее качество, всем советую этот продукт!",
+        "reviews_date": "2024-09-27",
+        "star_review": 5,
+        "text_len": 54,
+        "written_by_bot": 1,
+        "has_media": True
+    },
+    {
+        "user_review": "Заказываю не первый раз, всегда всё отлично.",
+        "reviews_date": "2024-09-26",
+        "star_review": 5,
+        "text_len": 49,
+        "written_by_bot": 1,
+        "has_media": False
+    },
+    {
+        "user_review": "Прекрасный продукт, ничего лучше не видел!",
+        "reviews_date": "2024-09-25",
+        "star_review": 5,
+        "text_len": 46,
+        "written_by_bot": 1,
+        "has_media": True
+    },
+    {
+        "user_review": "Очень доволен покупкой, всем рекомендую!",
+        "reviews_date": "2024-09-24",
+        "star_review": 5,
+        "text_len": 45,
+        "written_by_bot": 1,
+        "has_media": False
+    },
+    {
+        "user_review": "Продукт просто бомба! Никаких нареканий.",
+        "reviews_date": "2024-09-23",
+        "star_review": 5,
+        "text_len": 46,
+        "written_by_bot": 1,
+        "has_media": True
+    },
+    {
+        "user_review": "Не ожидал такого отличного качества! Закажу ещё.",
+        "reviews_date": "2024-09-22",
+        "star_review": 5,
+        "text_len": 54,
+        "written_by_bot": 1,
+        "has_media": True
+    },
+    {
+        "user_review": "Супер товар, рекомендую всем!",
+        "reviews_date": "2024-09-21",
+        "star_review": 5,
+        "text_len": 33,
+        "written_by_bot": 1,
+        "has_media": False
+    }
+]
