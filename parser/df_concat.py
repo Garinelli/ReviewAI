0c7a92eb1c351f81a9456e0eb5a6556aabfa67d5
acmPing <veselovskiydv@gmail.com>
2024-11-20 11:45:53 +0700

    Add using of absolute path in code

--- parser/df_concat.py
+++ parser/df_concat.py
@@ -1,15 +1,17 @@
+from pathlib import Path
 import pandas as pd
 
+path = Path(__file__).parent
 
-ozon_df = pd.read_csv("dataframe.csv")
-wb_df = pd.read_csv("feedbacks.csv")
+ozon_df = pd.read_csv(f"{path}/dataframe.csv")
+wb_df = pd.read_csv(f"{path}/feedbacks.csv")
 
 
-fake_reviews_1 = pd.read_csv("fake_feedbacks.csv")
-fake_reviews_2 = pd.read_csv("fake_reviews.csv")
+fake_reviews_1 = pd.read_csv(f"{path}/fake_feedbacks.csv")
+fake_reviews_2 = pd.read_csv(f"{path}/fake_reviews.csv")
 
 
 main_df = pd.concat([ozon_df, wb_df, fake_reviews_1, fake_reviews_2], axis=0)
-main_df.to_csv("main_df_raw.csv", index=False)  # Новый датафрейм
+main_df.to_csv(f"{path}/main_df_raw.csv", index=False)  # Новый датафрейм
 
 print("Dataframes concatenated successfully!")
