f4038bb6bfe8775a08ce8f1c267e902b88c64928
Yuri <yura.voskanyan.2003@mail.ru>
2025-03-17 12:57:47 +0700

    Files have been restored.

---
+++ src/parser/expansion_typos_reviews.py
@@ -0,0 +1,39 @@
+from random import randint
+import pandas as pd
+
+from reviews.reviews_with_typos import reviews
+
+user_reviews = []
+reviews_date = []
+star_reviews = []
+text_len = []
+written_by_bot = []
+has_media = []
+has_answer = []
+
+def main():
+    for review in reviews:
+        user_reviews.append(review['user_review'])
+        reviews_date.append(review['reviews_date'])
+        star_reviews.append(review['star_review'])
+        text_len.append(review['text_len'])
+        written_by_bot.append(review['written_by_bot'])
+        has_media.append(review['has_media'])
+        has_answer.append(randint(0, 1))
+    fake_dataset = pd.DataFrame(
+        {
+            'User review': user_reviews,
+            'Review date': reviews_date,
+            'Star review': star_reviews,
+            'Text length': text_len,
+            'Has media': has_media,
+            "Has answer": has_answer,
+            'Written by bot': written_by_bot,
+        }
+    )
+
+    fake_dataset.to_csv('./dataframes/typos_reviews.csv')
+
+
+if __name__ == '__main__':
+    main()
